266699a1a79f883295a6b4abad60ad309d310e5f
TeiturAllan <teiturdam@gmail.com>
2021-12-02 20:15:29 +0100

    created register function
    created and tested the register function.
    currently the registered user is saved into a variable inside the "server.js" file called "users", as i have not implemented a database yet

--- server.js
+++ server.js
@@ -1,9 +1,48 @@
+const { render, name } = require('ejs');
 const express = require('express');
 const app = express();
 
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false}))
+
+const users = []
+
+
 app.get('/', (req, res) => {
-    res.render(index.ejs)
+    res.render('index.ejs')
 });
+
+
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+    
+})
+
+
+
+app.post('/login', (req, res) => {
+    
+})
+
+
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+
+})    
+
+
+
+app.post('/register', (req, res) => {
+    users.push({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password 
+        })
+    res.redirect('/register')
+    console.log(users)
+})
 
 
 
